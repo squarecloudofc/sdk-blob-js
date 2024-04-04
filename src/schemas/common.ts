ba2eb0906e0d88ec6b05d3617ce1859ae1769d34
João Gabriel Tonaco <blu3zin@gmail.com>
2024-04-04 20:55:31 -0300

    add name like string schema

---
+++ src/schemas/common.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const stringSchema = z.string();
+
+export const nameLikeStringSchema = z
+	.string()
+	.min(3)
+	.max(32)
+	.regex(/^[a-zA-Z0-9_]{3,32}$/, {
+		message: "Name must contain only letters, numbers and _",
+	});
